13a01d72966b40ca38fffdeb50252349c519c0c3
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-06-03 17:18:53 +0200

    feature migration table Products and Seeder Project and Model Project

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Product;
+use Faker\Generator as Faker;
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+        for($i=0; $i < 10 ; $i ++){
+            $newProduct= new Product();
+            $newProduct->name=$faker->name();
+            $newProduct->brand=$faker->company();
+            $newProduct->slug=Str::slug($newProduct->name, '-');
+            $newProduct->description=$faker->text(300);
+            $newProduct->thumb='https://picsum.photos/200/300';
+            $newProduct->price=$faker->randomFloat(1, 1, 999);
+            $newProduct->save();
+         }
+    }
+}
